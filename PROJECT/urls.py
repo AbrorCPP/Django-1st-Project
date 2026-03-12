021ae98109b4fa80313f414f31251a82313a4843
AbrorCPP <abrorboltayev4843@gmail.com>
2026-03-12 18:45:08 +0500

    login request

--- PROJECT/urls.py
+++ PROJECT/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
     path(route="", view=views.home_page),
     path(route="contact/", view=views.contact_page),
     path(route="profile/", view=views.profile_page),
     path(route="login/", view=views.login_page),
+    path(route="authorize/", view=views.authorize),
 ]
--- PROJECT/views.py
+++ PROJECT/views.py
@@ -1,25 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 def home_page(request):
-    return render(
-        request,
-        'home_page.html',
-    )
+    return render( request,'home_page.html',)
 
 def contact_page(request):
-    return render(
-        request,
-        'contact.html',
-    )
+    return render( request,'contact.html',)
 
 def profile_page(request):
-    return render(
-        request,
-        'profile.html',
-    )
+    return render( request,'profile.html',)
 
 def login_page(request):
-    return render(
-        request,
-        'login.html',
-    )
+    return render( request,'login.html',)
+
+def authorize(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    if username == 'abror' and password == '12345':
+        return redirect("/")
+    else:
+        return redirect("/login")
